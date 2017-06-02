516e823d09c1dab15078989f4b5bb4259734d52b
aaoyekanmi4 <aaoyekanmi4@gmail.com>
2017-06-02 17:28:58 -0500

    clue project using sockets.io both players can pick characters and they are drawn on the game screen

---
+++ index.js
@@ -0,0 +1,51 @@
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+app.get('/', function(req, res){
+res.sendFile(__dirname + '/index.html');
+});
+
+var buttonState = {}
+io.on('connection', function(socket){
+var socket_ids = []
+
+
+     Object.keys(io.sockets.sockets);
+     Object.keys(io.sockets.sockets).forEach(function(id) {
+
+    if (socket_ids.indexOf(id) < 0){
+        socket_ids.push(id);
+    }
+
+
+});
+     buttonState[socket.id] = "off";
+     console.log(buttonState);
+     console.log(socket_ids);
+
+     io.emit('grabSocketId', socket_ids);
+
+
+
+
+
+socket.on('nameChosen', function(msg){
+  if (buttonState[socket.id] ==="off"){
+  buttonState[socket.id] = "on"
+    io.emit('nameChosen', msg);
+
+    console.log("1st time clicking button")
+  }
+  else {
+    io.to(socket.id).emit('alreadyPicked', "You've already picked a character")
+    console.log("2nd time clicking button")
+  }
+  });
+
+
+});
+
+http.listen(3000, function(){
+  console.log('listening on *:3000');
+});
